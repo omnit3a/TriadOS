fc45ab3789bad905ae7d42bd2942086d90a38996
omni-works <83828882+omnit3a@users.noreply.github.com>
2022-11-20 23:06:21 -0500

    Revert "Added basic keyboard support"
    This reverts commit 917ae78e20e8fea716f942eea68034c9b7649d78.

--- filesystem.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef FILESYSTEM_H_
-#define FILESYSTEM_H_
-
-#endif
--- message_bar.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MESSAGE_BAR_H_
-#define MESSAGE_BAR_H_
-
-#include "memory.hpp"
-
-extern "C" void displayMessageBar();
-
-#endif
